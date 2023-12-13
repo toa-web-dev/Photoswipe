5b183f6576b050738fb9ce8ea34b6df40fb211f8
gimHyoungSoo <gimhyoungsoo@gmail.com>
2023-12-13 15:13:31 +0900

    ✨ 선택 버튼에 스로틀 적용

--- src/app.js
+++ src/app.js
@@ -2,11 +2,25 @@
 import { Item } from "./components/Item.js";
 function app() {
     //쓰로틀 적용 예정
-    document.querySelector("#btn_like").onclick = () => {
-        swipe("like");
+    //쓰로틀로 이벤트 대기중일때는 버튼이 비활성화된 스타일을 보여줘야 UX 향상함
+    let timer = null;
+    const btnLike = document.querySelector("#btn_like");
+    const btnDislike = document.querySelector("#btn_dislike");
+    const thorttle = (func, el) => {
+        if (!timer) {
+            func();
+            el.classList.add("deactive");
+            timer = setTimeout(() => {
+                timer = null;
+                el.classList.remove("deactive");
+            }, 500);
+        }
     };
-    document.querySelector("#btn_dislike").onclick = () => {
-        swipe("dislike");
+    btnLike.onclick = () => {
+        thorttle(() => swipe("like"), btnLike);
+    };
+    btnDislike.onclick = () => {
+        thorttle(() => swipe("dislike"), btnDislike);
     };
 
     let $cardContainer = document.querySelector("#card_container");
@@ -14,8 +28,8 @@ function app() {
     $cardContainer.append(Item(), Item());
 
     current = $cardContainer.querySelector(".card:last-child");
-    const swipe = (action) => {
-        current.classList.add(action);
+    const swipe = (preference) => {
+        current.classList.add(preference);
         const prev = current;
         const next = current.previousElementSibling;
         // if (nextCard) initEventCard(nextCard);
