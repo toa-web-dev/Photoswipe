558cb6fb42816fb9b957567ac8c2149675bbfc59
gimHyoungSoo <gimhyoungsoo@gmail.com>
2023-12-13 16:31:30 +0900

    💡 데이터 페이지네이션을 위한 주석추가

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -1,4 +1,15 @@
 export async function getData(seed) {
     const data = await fetch(`https://picsum.photos/100/150?random=${seed}`);
-    return ;
+    return;
 }
+
+// fetch 기능
+/**
+ * 1. 렌더링할 5장의 데이터를 fetch하여 let currentCardDataList에 할당한다
+ * 2. currentCardDataList에서 map을 사용해 5장의 카드를 렌더링한다.
+ * 3. 다음에 쓰일 5장의 데이터를 fetch하여 let nextCardDataList에 할당한다
+ * 4. Item을 렌더링할때마다 let counter = 0을 counter++ 한다.
+ * 5. counter가 5가되면 currentCardDataList에 nextCardDataList를 할당한다.
+ * 6. counter = 0으로 초기화한다.
+ * 7. 2부터 반복한다
+ */
