738045645f0e27dda3733500b5218d02f9452d30
gimHyoungSoo <gimhyoungsoo@gmail.com>
2023-12-25 16:52:13 +0900

    🚧 카드 제거 로직 작업 중 - 카드가 제거될때의 transform값을 정하면 됨

--- src/app.js
+++ src/app.js
@@ -37,13 +37,15 @@ function app() {
             }
         };
     };
+
+    //카드 DOM요소를 추가하는 작업과 fetch한 배경이미지를 할당하는 작업이 분리 되어야 한다고 생각함
     const InitData = async () => {
         targetArr = await getCardDataFromId(cardIdArr); //[0,1,2,3,4]
         for (let i = targetArr.length - 1; i >= 0; i--) {
             $cardContainer.appendChild(Item(targetArr[i]));
         }
         $currentCard = $cardContainer.querySelector(".card:last-child");
-        InitPointerEvent();
+        initPointerEvent();
         cardIdArr = cardIdArr.map((el) => el + RENDER_CARD_NUM);
         bufferArr = await getCardDataFromId(cardIdArr);
     };
@@ -63,84 +65,83 @@ function app() {
     };
     const RemoveCard = (preference) => {
         try {
-            $currentCard.classList.add(`action_${preference}`);
+            // 아래 트랜스폼 값을 화면 밖으로 날아가게 바꾸면 됨
+            set$currentCardTransform(0, 0, 10, 0.5);
             const $prevCard = $currentCard;
             $currentCard = $currentCard.previousElementSibling;
             const RemoveSwipedCard = () => {
                 $cardContainer.removeChild($prevCard);
-                $prevCard.removeEventListener("animationend", RemoveSwipedCard);
+                $prevCard.removeEventListener("transitionend", RemoveSwipedCard);
             };
-            $prevCard.addEventListener("animationend", RemoveSwipedCard);
+            $prevCard.addEventListener("transitionend", RemoveSwipedCard);
         } catch (e) {
-            console.log("카드 추가 중 오류 발생", e);
+            console.log("카드 제거 중 오류 발생", e);
         }
     };
     const Swipe = (preference) => {
         InsertCard();
         RemoveCard(preference);
+        initPointerEvent();
     };
     InitBtn();
     InitData();
 
-    let startPoint = {};
-    const InitPointerEvent = () => {
-        console.log("init pointer event");
-        $currentCard.addEventListener("pointerdown", PointDown);
-        $currentCard.removeEventListener("pointermove", PointMove);
-        $currentCard.removeEventListener("pointerup", PointUp);
-        $currentCard.removeEventListener("pointerleave", PointLeave);
+    const set$currentCardTransform = (x = 0, y = 0, deg = 0, duration = null) => {
+        $currentCard.style.transform = `translate(${x}px,${y}px) rotate(${deg}deg)`;
+        if (duration) $currentCard.style.transition = `transform ${duration}s`;
     };
 
-    const PointDown = (e) => {
+    let startPoint = {},
+        movePoint = {},
+        distance = {};
+    const initPointerEvent = () => {
+        console.log("init pointer event");
+        $currentCard.addEventListener("pointerdown", pointerDown);
+        $currentCard.removeEventListener("pointermove", pointerMove);
+        $currentCard.removeEventListener("pointerup", pointerUp);
+        $currentCard.removeEventListener("pointerleave", pointerLeave);
+    };
+
+    const pointerDown = (e) => {
         console.log("pointer down");
         startPoint = { x: parseInt(e.clientX), y: parseInt(e.clientY) };
-        $currentCard.addEventListener("pointermove", PointMove);
-        $currentCard.addEventListener("pointerup", PointUp);
-        $currentCard.addEventListener("pointerleave", PointLeave);
+        $currentCard.addEventListener("pointermove", pointerMove);
+        $currentCard.addEventListener("pointerup", pointerUp);
+        $currentCard.addEventListener("pointerleave", pointerLeave);
     };
 
-    const PointMove = (e) => {
+    const pointerMove = (e) => {
         console.log("pointer move");
-        const distance = {
-            x: parseInt(e.clientX) - startPoint.x,
-            y: parseInt(e.clientY) - startPoint.y,
+        movePoint = { x: parseInt(e.clientX), y: parseInt(e.clientY) };
+        distance = {
+            x: movePoint.x - startPoint.x,
+            y: movePoint.y - startPoint.y,
         };
         //일차방정식의 기울기 slope
-        const slope = distance.y / distance.x ? distance.y / distance.x : 0;
-        console.log(`기울기는 ${distance.y} / ${distance.x}`, distance.y / distance.x);
-        console.log(`절편은 ${startPoint.y} - ${slope} * ${startPoint.x}`, startPoint.y - slope * startPoint.x);
-        const calcPosition = (x, y) => {
-            const equationIntercept = startPoint.y - slope * startPoint.x;
-            const result = {};
-            return result;
-        };
-        console.log(innerWidth);
-        $currentCard.style.transform = `translate(${distance.x}px, ${distance.y}px) rotate(${
-            (distance.x / innerWidth) * 30
-        }deg)`;
+        // const slope = distance.y / distance.x ? distance.y / distance.x : 0;
+        // console.log(`기울기는 ${distance.y} / ${distance.x}`, distance.y / distance.x);
+        // console.log(`절편은 ${startPoint.y} - ${slope} * ${startPoint.x}`, startPoint.y - slope * startPoint.x);
+        const rotateDeg = (distance.x / window.innerWidth) * 30;
+        set$currentCardTransform(distance.x, distance.y, rotateDeg);
     };
-    const PointLeave = () => {
+    const pointerLeave = () => {
         console.log("pointer leave");
+        initPointerEvent();
         // 스와이프 범위 미만에서 발동될 경우 원점으로 돌아감
-        $currentCard.style.transform = `translate(0,0)`;
+        set$currentCardTransform(0, 0);
         // 스와이프 범위 이상일 경우 OX 처리 함수 실행
-        InitPointerEvent();
     };
-    const PointUp = () => {
+    const pointerUp = () => {
         console.log("pointer up");
+        initPointerEvent();
+        if ($cardContainer.clientWidth / 2 < Math.abs(distance.x)) {
+            const preference = distance.x > 0 ? "like" : "dislike";
+            Swipe(preference);
+        } else set$currentCardTransform(0, 0);
+
         // 스와이프 범위 미만에서 발동될 경우 원점으로 돌아감
-        $currentCard.style.transform = `translate(0,0)`;
+
         // 스와이프 범위 이상일 경우 OX 처리 함수 실행
-        InitPointerEvent();
-    };
-
-    //카드가 넘어간 뒤 $currentCard에 다음 카드가 할당되었을 때 기존 이벤트리스너를 제거하고 새로 등록해야함
-
-    const remove = () => {
-        $currentCard.removeEventListener("pointerdown", PointDown);
-        $currentCard.removeEventListener("pointermove", PointMove);
-        $currentCard.removeEventListener("pointerup", PointUp);
-        $currentCard.removeEventListener("pointerleave", PointLeave);
     };
 }
 app();
